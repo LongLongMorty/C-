18bea32bacda072617058656c1dffaf3be0552a8
LongLongMorty <96741698+LongLongMorty@users.noreply.github.com>
2022-04-06 21:30:29 +0800

    Update and rename 20220406 to 20220406.cpp

--- 20220406.cpp
+++ 20220406.cpp
@@ -1,6 +1,5 @@
 洛谷P1614
 方法1：常规的指针移动
-```cpp
 #include <iostream>
 using namespace std;
 int a[10001] ;
@@ -28,5 +27,25 @@ int main()
     cout << min;
     return 0;
 }
-```
 方法2：前缀和//呜呜，没想到呢，太菜了
+#include <iostream>
+using namespace std;
+int a[10001] ,b[10001];
+int main()
+{
+    int n,m,min = 100000,ins = 0;
+    cin >> n >> m;
+    for(int i = 1;i <= n;i++)
+    {
+        cin >> a[i];
+        b[i] = b[i-1] + a[i];
+    }
+    for(int i = 0,j = m;j <= n;j++,i++)
+    {
+        ins = b[j] - b[i];
+        if(ins < min)
+            min = ins;
+    }
+    cout << min;
+    return 0;
+}//记得min初始化时开大一点
